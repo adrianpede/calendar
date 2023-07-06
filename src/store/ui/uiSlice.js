b0bf7cad67e8a38a61c2b2a8c43ec36805fe9918
Adrianpede <pedemonteadrian@gmail.com>
2023-07-05 23:43:04 -0300

    Instalacion y configuración de Redux

---
+++ src/store/ui/uiSlice.js
@@ -0,0 +1,22 @@
+
+
+import { createSlice } from '@reduxjs/toolkit';
+export const uiSlice = createSlice({ 
+name: 'ui', 
+ initialState: { 
+ isDateModalOpen: false
+ }, 
+ reducers: { 
+   onOpenDataModal: (state)=> {
+    state.isDateModalOpen = true
+
+   }, 
+   onCloseDataModal: (state)=> {
+    state.isDateModalOpen = false
+   }, 
+ } 
+});
+
+
+// Action creators are generated for each case reducer function
+export const { increment } = uiSlice.actions;
